564dcfac3442e645879b6a667ee430a80ac8aefc
Aron Sweet <sweet@andrews.edu>
2024-09-01 16:25:35 -0500

    Updated CSS

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,17 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  markdown: {
+    shikiConfig: {
+      themes: {
+        light: 'github-light',
+        dark: 'github-dark',
+      },
+      defaultColor: false,
+      langs: ['astro', 'javascript', 'typescript', 'html', 'css', 'markdown', 'go'],
+      wrap: true,
+      transformers: [],
+    },
+  }
 });
